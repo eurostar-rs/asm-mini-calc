7aab442a18e524aa55d397069576a3b290591fcf
GetALife <74838478+GetALife@users.noreply.github.com>
2020-11-30 00:06:29 +0200

    Add files via upload

--- assembly.asm
+++ assembly.asm
@@ -1,12 +1,22 @@
 %include "asm_io.inc"
+
+
 segment .data
+
+
 number1 db "Write the first number:",10,0
 number2 db "Write the second number:",10,0
 addition db "The result of the addition is: ",0
 subtraction db "The result of the subtraction is: ",0
 multiplication db "The result of multiplication is: ",0
 division db "The result of division is: ",0
+
+
 segment .bss
+
+no1   resd 1
+no2   resd 1
+
 
 segment .text
         global  asm_main
@@ -17,39 +27,40 @@ asm_main:
 
         mov eax,number1
         call print_string
-        mov ebx,eax
         call read_int
-        mov ebx,eax
+        mov [no1],eax
 
         mov eax,number2
         call print_string
         call read_int
-        mov ecx,eax
+        mov [no2],eax
 
         mov eax,addition
         call print_string
-        mov eax,ebx
-        add eax,ecx
+        mov eax,[no1]
+        add eax,[no2]
         call print_int
         call print_nl
 
         mov eax,subtraction
         call print_string
-        mov eax,ebx
-        sub eax,ecx
+        mov eax,[no1]
+        sub eax,[no2]
         call print_int
         call print_nl
 
         mov eax,multiplication
         call print_string
-        mov eax,ebx
+        mov eax,[no1]
+        mov ecx,[no2]
         mul ecx
         call print_int
         call print_nl
 
         mov eax,division
         call print_string
-        mov eax,ebx
+        mov eax,[no1]
+        mov ecx,[no2]
         div ecx
         call print_int
         call print_nl
